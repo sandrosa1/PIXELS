f38806a242cddf9d75c6f10b6131d8cbfce693e6
Sandro Amâncio de Sá <sandro.sa@fatec.sp.gov.br>
2020-06-18 18:33:50 -0300

    layout index

--- index.php
+++ index.php
@@ -1,97 +1,180 @@
 <?php include 'include/nossosLinksTop.php';?>
 <?php $page = 'home'; include 'include/header.php';?>
 <div class="container-fluid">
-<!--Inicio do Primeiro Layout-->
+  <!--Inicio do Primeiro Layout-->
+  <article>
     <div id="borda" class="row">
-        <div class="col-sm-7 div1">
-            <h1>Bienal de arte de Curitiba</h1>
-            <div class="text-center">
-                <img src="imagem/indexP.jpg" class="img-fluid" alt="Imagem responsiva">
-                <p class="text-justify">Lorem Ipsum é simplesmente uma simulação de texto
-                    da indústria tipográfica e de impressos, e vem sendo utilizado desde o
-                    século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e
-                    os embaralhou para fazer um livro de modelos.</p>
-            </div>
+      <div class="col-lg-7 indexdiv1">
+        <div class="card mb-3">
+          <h1 class="card-title">Bienal de Arte Digital de Curitíba</h1>
+          <img class="card-img-top" src="imagem/indexP.jpg" alt="Imagem de capa do card">
+          <div class="card-body">
+            <p class="card-text">Este é um card maior com suporte a texto embaixo, que funciona como uma introdução a um
+              conteúdo adicional. Este conteúdo é um pouco maior, para demonstração.</p>
+            <a href="#" class="card-link">Mais...</a>
+            <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
+          </div>
         </div>
-        <div class="col-sm-5 div2">
-            <ul class="list-unstyled">
-                <li class="media">
-                    <img class="mr-3" src="imagem/indexp1.jpg" alt="Imagem de exemplo genérica">
-                    <div class="media-body">
-                        <h5 class="mt-0 mb-1">Crie seu Desenho Detorial</h5>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
-                    </div>
-                </li>
-                <li class="media my-4">
-                    <img class="mr-3" src="imagem/indexp2.jpg" alt="Imagem de exemplo genérica">
-                    <div class="media-body">
-                        <h5 class="mt-0 mb-1">Pixel Art de volta</h5>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin..
-                    </div>
-                </li>
-                <li class="media">
-                    <img class="mr-3" src="imagem/indexp3.jpg" alt="Imagem de exemplo genérica">
-                    <div class="media-body">
-                        <h5 class="mt-0 mb-1">Tecnologia e Ilustração Digital</h5>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin..
-                    </div>
-                </li>
-            </ul>
+      </div>
+      <div class="col-lg-5 indexdiv2">
+        <div class="card mb-3">
+          <ul class="list-unstyled">
+            <li class="media">
+              <img class="mr-3" src="imagem/indexp1.jpg" alt="Imagem de exemplo genérica">
+              <div class="media-body">
+                <h2 class="mt-0 mb-1">Crie seu Desenho Detorial</h2>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
+                <a href="#" class="card-link">Saiba Mais</a>
+              </div>
+            </li>
+            <li class="media my-4">
+              <img class="mr-3" src="imagem/indexp2.jpg" alt="Imagem de exemplo genérica">
+              <div class="media-body">
+                <h2 class="mt-0 mb-1">Pixel Art de volta</h2>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin..
+                <a href="#" class="card-link">Saiba Mais</a>
+              </div>
+            </li>
+            <li class="media">
+              <img class="mr-3" src="imagem/indexp3.jpg" alt="Imagem de exemplo genérica">
+              <div class="media-body">
+                <h2 class="mt-0 mb-1">Tecnologia e Ilustração Digital</h2>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin..
+                <a href="#" class="card-link">Saiba Mais</a>
+              </div>
+            </li>
+            <li class="media">
+              <img class="mr-3" src="imagem/indexp3.jpg" alt="Imagem de exemplo genérica">
+              <div class="media-body">
+                <h2 class="mt-0 mb-1">Tecnologia e Ilustração Digital</h2>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin..
+                <a href="#" class="card-link">Saiba Mais</a>
+              </div>
+            </li>
+          </ul>
         </div>
+      </div>
     </div>
-<!--Inicio do segundo Layout 3 3 colunas-->
+  </article>
+  <!--//////////////////////////////////////                 /////////////////////////////////////////////////////-->
+  <!--Inicio do segundo Layout 3 3 colunas-->
+  <section>
     <div class="row">
-        <div class="col-sm">
-        <h5>Calendário de Eventos</h5>
-                <img src="imagem/indexp7calendario.jpg" class="figure-img img-fluid rounded"
-                    alt="Imagem de um quadrado genérico com bordas arredondadas, em uma figure.">
-                <figcaption class="figure-caption">Uma legenda para a imagem acima.</figcaption>
-            </figure>
+      <div class="col-lg-3">
+      <div class="card">
+    <img class="card-img" src="imagem/indexp7calendario.jpg" alt="Imagem do card">
+  </div>
+  <div>
+  <table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Evento</th>
+      <th scope="col">Data</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1ºExposição Digita</th>
+      <td>25/01/21</td>
+    </tr>
+    <tr>
+      <th scope="row">Anima Mundi</th>
+      <td>05/03/21</td>    
+    </tr>
+    <tr>
+      <th scope="row">BGS</th>
+      <td colspan="2">08/09/21</td>
+    </tr>
+    <tr>
+      <th scope="row">1ºExposição Digita</th>
+      <td>25/01/21</td>
+    </tr>
+    <tr>
+      <th scope="row">1ºExposição Digita</th>
+      <td>25/01/21</td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+  <div class="card">
+    <img class="card-img" src="imagem/logo500.png" alt="Imagem do card">
+  </div>
+      </div>
+      <div class="col-lg-9">
+      <div class="card-deck">
+      <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="imagem/indexp4.jpg" alt="Imagem de capa do card">
+  <div class="card-body">
+    <h5 class="card-title">Título do card</h5>
+    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+    <a href="#" class="btn btn-primary">Visitar</a>
+  </div>
+</div>
+      <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Título do card</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Subtitulo do card</h6>
+    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+    <a href="#" class="card-link">Outro link</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Título do card</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Subtitulo do card</h6>
+    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+    <a href="#" class="card-link">Link do card</a>
+    <a href="#" class="card-link">Outro link</a>
+  </div>
+</div>
+      </div>
+      <div class="card-deck">
+        <div class="card">
+          <img class="card-img-top" src="imagem/indexp1.jpg" alt="Imagem de capa do card"
+            style="height: 250px;">
+          <div class="card-body">
+            <h5 class="card-title">Título do card</h5>
+            <p class="card-text">Este é um card mais longo com suporte a texto embaixo,
+              que funciona como uma introdução a um conteúdo adicional. Este conteúdo é um pouco maior.</p>
+            <a href="#" class="card-link">Mais...</a>
+            <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
+          </div>
         </div>
-        <div class="col-sm">
-            <figure class="figure">
-                <h5>TITULO</h5>
-                <img src="imagem/indexp4.jpg" class="figure-img img-fluid rounded"
-                    alt="Imagem de um quadrado genérico com bordas arredondadas, em uma figure.">
-                <figcaption class="figure-caption">Uma legenda para a imagem acima.</figcaption>
-                <p class="text-justify">Lorem Ipsum é simplesmente uma simulação de texto
-                    da indústria tipográfica e de impressos, e vem sendo utilizado desde o
-                    século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e
-                    os embaralhou para fazer um livro de modelos.</p>
-            </figure>
-            <figure class="figure">
-                <h5>TITULO</h5>
-                <img src="imagem/indexp5.jpg" class="figure-img img-fluid rounded"
-                    alt="Imagem de um quadrado genérico com bordas arredondadas, em uma figure.">
-                <figcaption class="figure-caption">Uma legenda para a imagem acima.</figcaption>
-                <p class="text-justify">Lorem Ipsum é simplesmente uma simulação de texto
-                    da indústria tipográfica e de impressos, e vem sendo utilizado desde o
-                    século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e
-                    os embaralhou para fazer um livro de modelos.</p>
-            </figure>
+        <div class="card">
+          <img class="card-img-top" src="imagem/indexp5.jpg" alt="Imagem de capa do card">
+          <div class="card-body">
+            <h5 class="card-title">Título do card</h5>
+            <p class="card-text">Este é um card com suporte a texto embaixo, que funciona como uma introdução
+              a um conteúdo adicional.</p>
+            <a href="#" class="card-link">Mais...</a>
+            <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
+          </div>
         </div>
-        <div class="col-sm">
-            <h5>TITULO</h5>
-            <p class="text-justify">Lorem Ipsum é simplesmente uma simulação de texto
-                da indústria tipográfica e de impressos, e vem sendo utilizado desde o
-                século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e
-                os embaralhou para fazer um livro de modelos.</p>
-            <h5>TITULO</h5>
-            <p class="text-justify">Lorem Ipsum é simplesmente uma simulação de texto
-                da indústria tipográfica e de impressos, e vem sendo utilizado desde o
-                século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e
-                os embaralhou para fazer um livro de modelos.</p>
-                <figure class="figure">
-                <h5>TITULO</h5>
-                <img src="imagem/indexp6.jpg" class="figure-img img-fluid rounded"
-                    alt="Imagem de um quadrado genérico com bordas arredondadas, em uma figure.">
-                <figcaption class="figure-caption">Uma legenda para a imagem acima.</figcaption>
-                <p class="text-justify">Lorem Ipsum é simplesmente uma simulação de texto
-                    da indústria tipográfica e de impressos, e vem sendo utilizado desde o
-                    século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e
-                    os embaralhou para fazer um livro de modelos.</p>
-            </figure>
+        <div class="card">
+          <img class="card-img-top" src="imagem/indexp6.jpg" alt="Imagem de capa do card">
+          <div class="card-body">
+            <h5 class="card-title">Título do card</h5>
+            <p class="card-text">Este é um card maior com suporte a texto embaixo, que funciona como uma
+              introdução a um conteúdo adicional. Este card tem o conteúdo ainda maior que o primeiro,
+              para mostrar a altura igual, em ação.</p>
+            <a href="#" class="card-link">Mais...</a>
+            <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
+          </div>
         </div>
+      </div>
+      </div>
     </div>
+  </section>
+  <!--////                                                                                                        /-->
+  <!--FOOTER-->
+  <div class="row">
+    <div class="col-lg-12"></div>
+    <footer>
+      <p>&copy;Desenvolvido por Pixels<p>
+    </footer>
+  </div>
+</div>
+
+
 </div>
 <? include 'include/nossosLinksBotom.php'?>
