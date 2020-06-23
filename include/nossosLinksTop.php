ec92e0648311d2081949d39cdce1eadfe7b17649
Carol <carolkaji@gmail.com>
2020-06-22 21:41:42 -0300

    Favicon para o index

--- include/nossosLinksTop.php
+++ include/nossosLinksTop.php
@@ -4,15 +4,15 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="../imagem/favicon.ico" />
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap" rel="stylesheet">
     <!-- Bootstrap CSS -->
     <!--Esse css e para as demais paginas-->
+    <link rel="shortcut icon" href="../imagem/favicon.ico" />
     <link href="../css/bootstrap.min.css" rel="stylesheet" >
     <link href="../css/stylus.css" rel="stylesheet">
     <!--Esse css e para a pagina index-->
+    <link rel="shortcut icon" href="imagem/favicon.ico" />
     <link href="css/bootstrap.min.css" rel="stylesheet" >
     <link href="css/stylus.css" rel="stylesheet">
     <title>Pixels</title>
