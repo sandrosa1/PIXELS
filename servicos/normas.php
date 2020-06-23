0a132828e50031c731964d6b384041d9062e97d4
Carol <carolkaji@gmail.com>
2020-06-22 23:23:02 -0300

    Ajuste do include do footer, faltando tag php

--- servicos/normas.php
+++ servicos/normas.php
@@ -5,34 +5,30 @@ include '../include/header.php'; ?>
 <article class="container-fluid" id="inicio">
     <div class="row justify-content-center">
         <div class="col-sm-10 col-md-10 col-lg-10 ">
-        <h1>Normas de Uilização</h1>
-        <p>
-            Fica terminantemente proibido a publicação ou divulgação de qualquer conteúdo presente no site do Pixels sem devida autorização e atruibuição de créditos.
-        </p>
-        <p>
-            Caso as regras forem descumpridas, comprovando ilegalidade, nos reservamos o direito de excluir o produto interativo.
-        </p>
-        <p>
-            O conteúdo publicado no presente website não deve conter os seguintes itens:
-            <ul>
-                <li>Material pornográfico ou atividades ilegais incluindo menores de 18 anos (segundo o artigo 241 do Estatuto da Criança e do Adolescente);</li>
-                <li>Material e/ou linguagem grosseira ou ofensiva;</li>
-                <li>Material calunioso, abusivo ou que invada a privacidade de alguém;</li>
-                <li>Informação sobre atividades ilegais e incitação ao crime;</li>
-                <li>Páginas e arquivos criptografados ou protegidos por senhas</li>
-                <li>Material que configure crime virtual</li>
-                <li>Programas e arquivos que contenham vírus ou qualquer outro código malicioso;</li>
-            </ul>
-        </p>
-        <p>
-        Aplicam-se ainda aos usuários dos serviços da Pixels, as Normas de Segurança e Privacidade.
-        </p>
+            <h1>Normas de Uilização</h1>
+            <p>
+                Fica terminantemente proibido a publicação ou divulgação de qualquer conteúdo presente no site do Pixels sem devida autorização e atruibuição de créditos.
+            </p>
+            <p>
+                Caso as regras forem descumpridas, comprovando ilegalidade, nos reservamos o direito de excluir o produto interativo.
+            </p>
+            <p>
+                O conteúdo publicado no presente website não deve conter os seguintes itens:
+                <ul>
+                    <li>Material pornográfico ou atividades ilegais incluindo menores de 18 anos (segundo o artigo 241 do Estatuto da Criança e do Adolescente);</li>
+                    <li>Material e/ou linguagem grosseira ou ofensiva;</li>
+                    <li>Material calunioso, abusivo ou que invada a privacidade de alguém;</li>
+                    <li>Informação sobre atividades ilegais e incitação ao crime;</li>
+                    <li>Páginas e arquivos criptografados ou protegidos por senhas</li>
+                    <li>Material que configure crime virtual</li>
+                    <li>Programas e arquivos que contenham vírus ou qualquer outro código malicioso;</li>
+                </ul>
+            </p>
+            <p>
+                Aplicam-se ainda aos usuários dos serviços da Pixels, as Normas de Segurança e Privacidade.
+            </p>
         </div>
-
-       
     </div>
-
-
 </article>
-<? include '../include/footer.php'?>
-<? include '../include/nossosLinksBotom.php'?>
+<?php include '../include/footer.php'?>
+<?php include '../include/nossosLinksBotom.php' ?>
