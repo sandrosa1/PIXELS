5f23932c0df7f2bd59b1c48fc0ee1694eeaa4b6e
Carol <carolkaji@gmail.com>
2020-06-23 15:58:39 -0300

    Icones para midia social

--- include/nossosLinksTop.php
+++ include/nossosLinksTop.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.1/css/all.css" integrity="sha384-xxzQGERXS00kBmZW/6qxqJPyxW3UR0BPsL4c8ILaIWXva5kFi7TxkIIaMiKtqV1Q" crossorigin="anonymous">
     <!-- Bootstrap CSS -->
     <!--Esse css e para as demais paginas-->
     <link rel="shortcut icon" href="../imagem/favicon.ico" />
--- servicos/quemSomos.php
+++ servicos/quemSomos.php
@@ -14,9 +14,9 @@ include '../include/header.php'; ?>
                     <p class="card-text">Grupo de desenvolvedores multidisciplinares, estudantes do 2º semestre da faculdade de tecnologia FATEC São Roque.</p>
                     <p>Fundado em meados de 2020, em meio à pandemia de COVID-19, o grupo se juntou com o intuito de trazer conhecimento e diversão através de um portal sobre diversas formas de artes digitais.</p>
                 </div>
-                <div class="card-footer">
+                <!-- <div class="card-footer">
                     <small class="text-muted">AAAAAAAAA</small>
-                </div>
+                </div> -->
             </div>
             <!-- CARD do desenvolvedor Animação Digital -->
             <div class="card dev1">
@@ -27,6 +27,22 @@ include '../include/header.php'; ?>
                     <p class="text-center mt-1"> Função no desenvolvimento do portal</p>
                     <hr>
                     <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    <p>
+                        <ul class="ul-inline animacao">
+                            <!-- Font Awesome - Linkedin -->
+                            <li>
+                                <a href="" target="_blank"><i class="fab fa-linkedin"></i></a>
+                            </li>
+                            <!-- Font Awesome - Github -->
+                            <li>
+                                <a href="" target="_blank"><i class="fab fa-github-square"></i></a>
+                            </li>
+                            <!-- Font Awesome - Instagram -->
+                            <li>
+                                <a href="" target="_blank"><i class="fab fa-instagram-square"></i></a>
+                            </li>
+                        </ul>
+                    </p>
                 </div>
                 <div class="card-footer">
                     <small class="text-muted">
@@ -44,6 +60,22 @@ include '../include/header.php'; ?>
                     <p class="text-center mt-1"> Função no desenvolvimento do portal</p>
                     <hr>
                     <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    <p>
+                        <ul class="ul-inline desenho">
+                            <!-- Font Awesome - Linkedin -->
+                            <li>
+                                <a href="" target="_blank"><i class="fab fa-linkedin"></i></a>
+                            </li>
+                            <!-- Font Awesome - Github -->
+                            <li>
+                                <a href="" target="_blank"><i class="fab fa-github-square"></i></a>
+                            </li>
+                            <!-- Font Awesome - Instagram -->
+                            <li>
+                                <a href="" target="_blank"><i class="fab fa-instagram-square"></i></a>
+                            </li>
+                        </ul>
+                    </p>
                 </div>
                 <div class="card-footer">
                     <small class="text-muted">
@@ -55,7 +87,7 @@ include '../include/header.php'; ?>
     </div>
     <div class="row no-margin justify-content-center mt-5 mb-5">
         <div class="card-deck max-width ">
-            
+
             <!-- CARD do desenvolvedor Ilustração Digital -->
             <div class="card dev3">
                 <img class="card-img-top" src="../imagem/user.png" alt="Card image cap">
@@ -65,6 +97,22 @@ include '../include/header.php'; ?>
                     <p class="text-center mt-1"> Função no desenvolvimento do portal</p>
                     <hr>
                     <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    <p>
+                        <ul class="ul-inline ilustracao">
+                            <!-- Font Awesome - Linkedin -->
+                            <li>
+                                <a href="" target="_blank"><i class="fab fa-linkedin"></i></a>
+                            </li>
+                            <!-- Font Awesome - Github -->
+                            <li>
+                                <a href="" target="_blank"><i class="fab fa-github-square"></i></a>
+                            </li>
+                            <!-- Font Awesome - Instagram -->
+                            <li>
+                                <a href="" target="_blank"><i class="fab fa-instagram-square"></i></a>
+                            </li>
+                        </ul>
+                    </p>
                 </div>
                 <div class="card-footer">
                     <small class="text-muted">
@@ -82,6 +130,24 @@ include '../include/header.php'; ?>
                     <p class="text-center mt-1"> Função no desenvolvimento do portal</p>
                     <hr>
                     <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    <p>
+                        <p>
+                            <ul class="ul-inline modelagem">
+                                <!-- Font Awesome - Linkedin -->
+                                <li>
+                                    <a href="" target="_blank"><i class="fab fa-linkedin"></i></a>
+                                </li>
+                                <!-- Font Awesome - Github -->
+                                <li>
+                                    <a href="" target="_blank"><i class="fab fa-github-square"></i></a>
+                                </li>
+                                <!-- Font Awesome - Instagram -->
+                                <li>
+                                    <a href="" target="_blank"><i class="fab fa-instagram-square"></i></a>
+                                </li>
+                            </ul>
+                        </p>
+                    </p>
                 </div>
                 <div class="card-footer">
                     <small class="text-muted">
@@ -100,6 +166,22 @@ include '../include/header.php'; ?>
                     <hr>
                     <p class="card-text">A paixão por desenvolvimento web client-side é algo que a faz se mover em direção à carreira que tanto almeja. E se tornar desenvolvedora Front-end, é seu objetivo.</p>
                     <p>Ela é mãe de um coelhinho chamado Tofu.</p>
+                    <p>
+                        <ul class="ul-inline pixel">
+                            <!-- Font Awesome - Linkedin -->
+                            <li>
+                                <a href="https://www.linkedin.com/in/carolina-kaji-88520915b" target="_blank"><i class="fab fa-linkedin"></i></a>
+                            </li>
+                            <!-- Font Awesome - Github -->
+                            <li>
+                                <a href="https://github.com/carolinakaji" target="_blank"><i class="fab fa-github-square"></i></a>
+                            </li>
+                            <!-- Font Awesome - Instagram -->
+                            <li>
+                                <a href="https://www.instagram.com/carol85_k/" target="_blank"><i class="fab fa-instagram-square"></i></a>
+                            </li>
+                        </ul>
+                    </p>
                 </div>
                 <div class="card-footer">
                     <small class="text-muted">
