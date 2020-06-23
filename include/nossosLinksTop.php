69c325ab6d489f993d6b58df6ec24cb8d5ee78b2
Carol <carolkaji@gmail.com>
2020-06-22 21:25:39 -0300

    Inlcusão da Fonte Raleway do Google Fonts

--- include/nossosLinksTop.php
+++ include/nossosLinksTop.php
@@ -4,6 +4,10 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="../imagem/favicon.ico" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap" rel="stylesheet">
     <!-- Bootstrap CSS -->
     <!--Esse css e para as demais paginas-->
     <link href="../css/bootstrap.min.css" rel="stylesheet" >
