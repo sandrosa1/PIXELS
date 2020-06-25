82aac5272b2cbbb840ba04a36cbc217c421543f2
Sandro Amâncio de Sá <sandro.sa@fatec.sp.gov.br>
2020-06-25 00:02:19 -0300

    Pagina sobre animação

--- animacao/animacaoSobre.php
+++ animacao/animacaoSobre.php
@@ -2,7 +2,129 @@
 <?php $page = 'animacao'; include '../include/header.php';?>
 <!--A Pagina tem que estar dentro do container-fluid para nõa quebrar-->
 <div class="container-fluid" id="inicio">
-    <h1>Sobre a Animação Digital</h1>
+    <article>
+        <div class="container">
+            <h1 class=" text-center ">História da Animação Digital</h1>
+            <div class="row">
+                <div class="col-md-12">
+                    <img class="img-fluid  align-items-center" alt="Ivan Sutherland e sua caneta óptica"
+                        style='float:left;width:500px; margin-right:20px;' src="../imagem/animacaoInicio.jpg" />
+                    <p class=" text-justify">A história da animação digital está diretamente relacionada com
+                        a história da computação gráfica. Desde os primeiros dispositivos
+                        disponíveis foram percebidas as possibilidade de uso para geração
+                        de ilusão de movimento. Em 1962 Ivan Sutherland desenvolveu uma “caneta óptica”
+                        com a qual imagens poderiam ser esboçadas em um computador.
+                        O primeiro programa feito por Sutherland que permitia a realização de projetos
+                        com a ajuda de computador, chamado Sketchpad, permitia que os projetistas usassem
+                        computadores para criar projetos de automóveis, cidades e produtos indústriais.
+                        class=" text-center " A imagem mostra Ivan Sutherland demonstrando o sistema “Sketchpad” no
+                        console
+                        do imenso computador TX-2 no MIT em 1963. O grande “salto” da computação gráfica
+                        foi com o surgimento da tecnologia de circuitos integrados e, posteriormente, na
+                        década de 80, o lançamento dos primeiros computadores pessoais de baixo custo, como
+                        o IBM-PC e o Macintosh da Apple. Em meados da década de 80, o novo terminal gráfico
+                        de varredura (raster graphics displays) foi a tecnologia que popularizou de vez o uso
+                        de gráficos nos computadores pessoais, tecnologia esta que possibilitou o desenvolvimento
+                        de inúmeros aplicativos baratos e fáceis de usar, tais como processadores de textos e planilhas
+                        eletrônicas. Nessas interfaces gráficas, o usuário conseguia posicionar janelas que atuavam como
+                        terminais virtuais, cada qual executando aplicativos independentes, permitindo assim que ele
+                        executasse
+                        vários aplicativos simultaneamente. O aprimoramento da tecnologia raster e o surgimento de
+                        placas aceleradoras gráficas permitiu na década de 90 a geração de imagens com alto grau
+                        de realismo em tempos real, favorecendo assim o surgimento de novas tecnologias nas áreas
+                        de jogos e efeitos especiais.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class=" text-center ">Inicio no Cinema</h2>
+                    <img class="img-fluid  align-items-center" alt="Exterminador do futuro"
+                        style='float:right;width:300px; margin-left:20px;' src="../imagem/animacaoTerminator1984.jpg" />
+                    <p class=" text-justify">No cinema, ela surgiu, como recurso para criar efeitos especiais
+                        que nos aproximassem cada vez mais com a realidade. Por exemplo: “Exterminador do futuro”,
+                        “Independence day”, “Armageddon”, “Matrix”, “Star Wars”, “Titanic”, “Godzilla”.
+                        Este recurso cresceu muito e começaram a ser criados filmes totalmente digitais, destinados
+                        principalmente ao público infantil, pelo fato de ter como temática assuntos do imaginário.
+                        Uma animação em especial que teve o objetivo de alcançar o realismo, utilizando-se da técnica de
+                        animação foto-realística, foi o “Final Fantasy” de Hironobu Sakaguchi, tem um cenário e
+                        personagens com um realismo impressionante que nos confunde se o que estamos assistindo é mesmo
+                        uma animação.</p>
+                    <h3 class=" text-center ">Alguns dos grandes longas de animação digital</h3>
+                    <p class=" text-justify">– “Toy Story”: dirigido por Jonh Lasseter, Roteiro de Andrew Stanton, Rita
+                        Hsiao, Doug Chamberlin, Chris Webb, produzido pela Pixar.</p>
+                    <p class=" text-justify">– “Madagascar”: direção Eric Darnell e Tom McGrath, produzido pela
+                        DreamWorks.</p>
+                    <p class=" text-justify">– “Sherk”: dirigito por Andrew Adamson e Vicky Jenson, roteiristas William
+                        Steig, Terry Rossio e Ted Elliott, produzido pela Dream Works.</p>
+                    <p class=" text-justify">– “Procurando Nemo”: diretor e roteirista Andrew Stanton, produzido pela
+                        Pixar.</p>
+                    <p class=" text-justify">– “Ratatouille”: direção Jan Pinkava e Brad Bird, roteiro Emily Cook, Kathy
+                        Greenberg, distribuído pela Pixar.</p>
+                    <p class=" text-justify">– “Bee Movie- a história de uma abelha”: direção Steve Hickner, Simon J.
+                        Smith, Dream Works.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card-deck">
+                        <div class="card">
+                            <img class="card-img-top" src="../imagem/animacaoTinToy.jpg"
+                                alt="Tin toy um dos primeiros curta de animaçõa">
+                            <div class="card-body">
+                                <h5 class="card-title">Tin Toy</h5>
+                                <p class="card-text">Tin Toy é um curta-metragem animado por computador feito pela Pixar
+                                    Animation Studios e dirigido por John Lasseter. Este curta pode ser assistido pela
+                                    internet, no antigo VHS Tiny Toy Stories de 1996, Pixar Short Films Collection -
+                                    Volume 1 ou como extra na fita de Toy Story lançada em 2000. Foi o vencedor do Oscar
+                                    na categoria de melhor curta animado em 1988.</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img class="card-img-top" src="../imagem/animacaoToyHistory.jpg"
+                                alt="Toy history uma das mais famosas animações">
+                            <div class="card-body">
+                                <h5 class="card-title">Toy Story</h5>
+                                <p class="card-text">Toy Story é um filme de animação, aventura e comédia americano
+                                    lançado em
+                                    1995. É conhecido por ser o primeiro filme da história do cinema a ter sido
+                                    compilado inteiramente por ferramentas de computação gráfica.</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img class="card-img-top" src="../imagem/animacaoFrozen.jpg" alt="Imagem de capa do card">
+                            <div class="card-body">
+                                <h5 class="card-title">Frozen</h5>
+                                <p class="card-text">Frozen foi recebido com
+                                    aclamação pela crítica e público em geral. O filme foi considerado a melhor animação
+                                    do estúdio desde a era do Renascimento da Disney. Arrecadou mais de 1,2 bilhão de
+                                    dólares nas bilheterias mundiais. É o terceiro filme original (que não é
+                                    continuação) de maior bilheteria, a terceira maior bilheteria na história do Japão.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                    <h4 class=" text-center ">A carreira do Designer De Animação</h4>
+                        <img class="img-fluid  align-items-center" alt="Ivan Sutherland e sua caneta óptica"
+                            style='float:left;width:400px; margin-right:20px;' src="../imagem/animacaoTrabalhando.jpg" />
+                        <p class=" text-justify">O mercado de trabalho oferece diversas oportunidades de atuação para a
+                            carreira de Designer de Animação, sendo crescente o número de festivais de animação tanto no
+                            âmbito nacional como na indústria internacional. Além de prestar serviços de forma
+                            independente, o designer pode atuar numa das mais de 350 agências que trabalham com animação
+                            no Brasil, atuando com cinema de animação, produção de videoclipes, animação de games,
+                            publicidade, Animação Web, design digital.
+                            Esse profissional é responsável por produzir e dirigir projetos de animação para as mais
+                            diversas plataformas digitais de comunicação; realizar edição audiovisual; criar cenários e
+                            ambientes virtuais; desenvolver animações em stop motion, trabalhar com efeitos especiais;
+                            desenvolver argumentos e roteiros de animação; atuar com criação de desenhos, design de
+                            personagens e direção de arte de projetos de animação e muito mais.</p>
+                    </div>
+                </div>
+    </article>
+
+
 
 
 </div>
