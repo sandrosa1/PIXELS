887c0495727aaf3160fee0113bba11388a7eb1d0
Sandro Amâncio de Sá <sandro.sa@fatec.sp.gov.br>
2020-07-03 08:20:40 -0300

    iniciando pagina homeAnimacao

--- animacao/animacao.php
+++ animacao/animacao.php
@@ -2,15 +2,163 @@
 <?php $page = 'animacao' ; include '../include/header.php'; ?>
 <!--A Pagina tem que estar dentro do container-fluid para nõa quebrar-->
 <div class="container-fluid animacaoHome1 " id="inicio">
-<header>
-<img src="../imagem/animacao/home.webp" class="img-fluid" alt="Logo da pagina Portal Animação digital" style="width: 100%;">
-</header>
-<article>
-
-</article>
-<section>
-        <?php include '../include/section.php'?>
-</section>
+        <header class="headerAni">
+                <img src="../imagem/animacao/home.webp" class="img-fluid" alt="Logo da pagina Portal Animação digital">
+        </header>
+        <article class="animacaoHomeP">
+                <div class="row">
+                        <div class=" col-lg-6 ">
+                                <div class="container1">
+                                        <img src="../imagem/animacao/animacaoInicio.webp" class="imgInicio" alt="#">
+                                        <div class="content1">
+                                                <h1>Historia da Animação</h1>
+                                                <p>Desde os primeiros dispositivos disponíveis foram percebidas as
+                                                        possibilidade de uso para geração de ilusão de movimento. </p><a
+                                                        href="animacao.php" class=" linksAnimacao ">Saiba mais...</a>
+                                        </div>
+                                </div>
+                                <div class="row">
+                                        <div class=" col-lg-12">
+                                                <div class="card-deck">
+                                                        <div class="card">
+                                                                <img class="card-img-top"
+                                                                        src="../imagem/animacao/animacaoJohnAlanLasseter.webp"
+                                                                        alt="John Alan Lasseter com dois personagens do toy story na mão">
+                                                                <div class="card-body">
+                                                                        <h5 class="card-title">John Alan Lasseter</h5>
+                                                                        <p class="card-text">Depois de ser demitido da
+                                                                                Disney por promover a animação
+                                                                                computadorizada</p><a
+                                                                                href="animacao.php"
+                                                                                class=" linksAnimacao1 ">Saiba
+                                                                                mais...</a>
+                                                                </div>
+                                                        </div>
+                                                        <div class="card">
+                                                                <img class="card-img-top"
+                                                                        src="../imagem/animacao/animacaoCap.webp"
+                                                                        alt="Imagem de capa do card">
+                                                                <div class="card-body">
+                                                                        <h5 class="card-title">Tecnicas de Animação</h5>
+                                                                        <p class="card-text">Conheça as principais
+                                                                                tecnicas de Animação.</p>
+                                                                        <a href="animacao.php"
+                                                                                class=" linksAnimacao1 ">Saiba
+                                                                                mais...</a>
+                                                                </div>
+                                                        </div>
+                                                        <div class="card">
+                                                                <img class="card-img-top" src=".../100px200/"
+                                                                        alt="Imagem de capa do card">
+                                                                <div class="card-body">
+                                                                        <h5 class="card-title">Título do card</h5>
+                                                                        <p class="card-text">Este é um card maior com
+                                                                                suporte a texto embaixo, que funciona
+                                                                                como uma introdução a</p>
+                                                                        <p class="card-text"><small
+                                                                                        class="text-muted">Atualizados 3
+                                                                                        minutos atrás</small></p>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                        </div>
+                                </div>
+                        </div>
+                        <div class=" col-lg-6 ">
+                                <div class="card-columns">
+                                        <div class="card">
+                                                <img class="card-img-top" src=".../100px160/"
+                                                        alt="Imagem de capa do card">
+                                                <div class="card-body">
+                                                        <h5 class="card-title">Título do card que quebra em uma nova
+                                                                linha</h5>
+                                                        <p class="card-text">Este é um card mais longo com suporte a
+                                                                texto embaixo, que funciona como uma introdução a um
+                                                                conteúdo adicional. Este conteúdo é um pouco maior.</p>
+                                                </div>
+                                        </div>
+                                        <div class="card p-3">
+                                                <blockquote class="blockquote mb-0 card-body">
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                                                Integer posuere erat a ante.</p>
+                                                        <footer class="blockquote-footer">
+                                                                <small class="text-muted">
+                                                                        Alguém famoso em <cite
+                                                                                title="Título da fonte">Título da
+                                                                                fonte</cite>
+                                                                </small>
+                                                        </footer>
+                                                </blockquote>
+                                        </div>
+                                        <div class="card">
+                                                <img class="card-img-top" src=".../100px160/"
+                                                        alt="Imagem de capa do card">
+                                                <div class="card-body">
+                                                        <h5 class="card-title">Título do card</h5>
+                                                        <p class="card-text">Este é um card com suporte a texto embaixo,
+                                                                que funciona como uma introdução a um conteúdo
+                                                                adicional.</p>
+                                                        <p class="card-text"><small class="text-muted">Atualizados 3
+                                                                        minutos atrás</small></p>
+                                                </div>
+                                        </div>
+                                        <div class="card bg-primary text-white text-center p-3">
+                                                <blockquote class="blockquote mb-0">
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                                                Integer posuere erat.</p>
+                                                        <footer class="blockquote-footer">
+                                                                <small>
+                                                                        Alguém famoso em <cite
+                                                                                title="Título da fonte">Título da
+                                                                                fonte</cite>
+                                                                </small>
+                                                        </footer>
+                                                </blockquote>
+                                        </div>
+                                        <div class="card text-center">
+                                                <div class="card-body">
+                                                        <h5 class="card-title">Título do card</h5>
+                                                        <p class="card-text">Este é um card com suporte a texto embaixo,
+                                                                que funciona como uma introdução a um conteúdo
+                                                                adicional.</p>
+                                                        <p class="card-text"><small class="text-muted">Atualizados 3
+                                                                        minutos atrás</small></p>
+                                                </div>
+                                        </div>
+                                        <div class="card">
+                                                <img class="card-img" src=".../100px260/" alt="Imagem do card">
+                                        </div>
+                                        <div class="card p-3 text-right">
+                                                <blockquote class="blockquote mb-0">
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                                                Integer posuere erat a ante.</p>
+                                                        <footer class="blockquote-footer">
+                                                                <small class="text-muted">
+                                                                        Alguém famoso em <cite
+                                                                                title="Título da fonte">Título da
+                                                                                fonte</cite>
+                                                                </small>
+                                                        </footer>
+                                                </blockquote>
+                                        </div>
+                                        <div class="card">
+                                                <div class="card-body">
+                                                        <h5 class="card-title">Título do card</h5>
+                                                        <p class="card-text">Este é um card maior com suporte a texto
+                                                                embaixo, que funciona como uma introdução a um conteúdo
+                                                                adicional. Este card tem o conteúdo ainda maior que o
+                                                                primeiro, para mostrar a altura igual, em ação.</p>
+                                                        <p class="card-text"><small class="text-muted">Atualizados 3
+                                                                        minutos atrás</small></p>
+                                                </div>
+                                        </div>
+                                </div>
+                        </div>
+                </div>
+        </article>
+        <section>
+                <?php include '../include/section.php'?>
+        </section>
 </div>
 <?php include '../include/footer.php'?>
 <?php include '../include/nossosLinksBotom.php' ?>
