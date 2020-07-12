8f5c9fe86150e3cf0c269b03e62086717dbc25f8
Carolina <carolina.kaji@gmail.com>
2020-07-12 15:40:09 -0300

    Inserido tag alt nas imagens, classe no-margin na linha de imagens e h1-arte para o título

--- pixel/pixelGaleria.php
+++ pixel/pixelGaleria.php
@@ -2,84 +2,85 @@
 <?php $page = 'pixel';
 include '../include/header.php'; ?>
 <!--A Pagina tem que estar dentro do container-fluid para nõa quebrar-->
-<div class="container-fluid pixel-index mx-auto" id="inicio">
-  <div class="row">
+<div class="container-fluid container-max mx-auto" id="inicio">
+  <div class="row no-margin">
     <div class="col-sm-12">
-      <h1 class="text-center m-3">Galeria de Artes</h1>
+      <h1 class="text-center m-3 h1-arte">Galeria de Artes</h1>
       <p class="text-center m-3">Confira a seleção de imagens de diversos artistas.</p>
     </div>
-    <section class="galeria-pixel">
+    <section class="galeria-pixel px-3">
       <div class="row no-margin text-center justify-content-center">
 
         <div class="ilust">
           <div class="align-self-end overlay">
             <figure>
-              <img class="img-fluid" src="../imagem/pixel/galeria/galeria-01.webp">
+              <img class="img-fluid" src="../imagem/pixel/galeria/galeria-01.webp" alt="David Bowie em pixel art">
               <figure class="text-center pt-1">David Bowie de Ivan Dixon</figure>
             </figure>
           </div>
           <div class="align-self-end overlay">
             <figure>
-              <img class="img-fluid align-self-center" src="../imagem/pixel/galeria/galeria-03.webp">
+              <img class="img-fluid align-self-center" src="../imagem/pixel/galeria/galeria-03.webp" alt="Mascotes em pixel art">
               <figcaption class="text-center pt-1">Mascots de Paul Robertson</figcaption>
             </figure>
           </div>
           <div class="align-self-end overlay">
             <figure>
-              <img class="img-fluid" src="../imagem/pixel/pixelforest.webp">
-              <figcaption class="text-center pt-1">Filme Pixels, de Chris Columbus</figcaption>
+              <img class="img-fluid" src="../imagem/pixel/pixelforest.webp" alt="Pixel Forest com pendentes de luzes coloridas">
+              <figcaption class="text-center pt-1">Pixel Forest</figcaption>
             </figure>
           </div>
         </div>
         <div class="ilust">
           <div class="align-self-end overlay">
             <figure>
-              <img class="img-fluid" src="../imagem/pixel/galeria/galeria-06.webp">
+              <img class="img-fluid" src="../imagem/pixel/galeria/galeria-06.webp" alt="Desenho Rick e Morty">
               <figcaption class="text-center pt-1">Rick & Morty de Ivan Dixon</figcaption>
             </figure>
           </div>
           <div class="align-self-end overlay">
             <figure>
-              <img class="img-fluid" src="../imagem/pixel/galeria/galeria-07.webp">
+              <img class="img-fluid" src="../imagem/pixel/galeria/galeria-07.webp" alt="Três espiãs demais em pixel art">
               <figcaption class="text-center pt-1">3 espiãs demais de Bruno Moraes</figcaption>
             </figure>
           </div>
           <div class="align-self-end overlay">
             <figure>
-              <img class="img-fluid" src="../imagem/pixel/galeria/galeria-09.webp">
+              <img class="img-fluid" src="../imagem/pixel/galeria/galeria-09.webp" alt="Cidade de LA com restaurantes">
               <figcaption class="text-center pt-1">Guia de restaurante de L.A. de eBoy</figcaption>
             </figure>
           </div>
           <div class="align-self-center overlay">
-            <img class="img-fluid" src="../imagem/pixel/galeria/galeria-05.webp">
-            <figcaption class="text-center pt-1">Jogo Chasm de Glauber Kotaki</figcaption>
-            <figure></figure>
+            <figure>
+              <img class="img-fluid" src="../imagem/pixel/galeria/galeria-05.webp" alt="Imagem uma cachoeira caindo sobre uma cruz">
+              <figcaption class="text-center pt-1">Jogo Chasm de Glauber Kotaki</figcaption>
+            </figure>
           </div>
         </div>
         <div class="ilust">
           <div class="align-self-end overlay ">
             <figure>
-              <img class="img-fluid align-self-center" src="../imagem/pixel/galeria/galeria-04.webp">
+              <img class="img-fluid align-self-center" src="../imagem/pixel/galeria/galeria-04.webp" alt="Mockup de um jogo em pixel art">
               <figcaption class="text-center pt-1">Game Mockup de Bruno Moraes</figcaption>
             </figure>
           </div>
           <div class="align-self-end overlay">
             <figure>
-              <img class="img-fluid" src="../imagem/pixel/galeria/galeria-11.webp">
+              <img class="img-fluid" src="../imagem/pixel/galeria/galeria-11.webp" alt="Torneira gigante na calçada com água em pixel art">
               <figcaption class="text-center pt-1">Escultura de Kelly Goelle</figcaption>
             </figure>
           </div>
 
           <div class="align-self-end overlay">
             <figure>
-              <img class="img-fluid" src="../imagem/pixel/galeria/galeria-13.webp">
+              <img class="img-fluid" src="../imagem/pixel/galeria/galeria-13.webp" alt="Outra imagem de David Bowie">
               <figcaption class="text-center pt-1">David Bowie de Bruno Moaes</figcaption>
             </figure>
           </div>
 
           <div class="align-self-end overlay">
             <figure>
-              <img class="img-fluid" src="../imagem/pixel/galeria/galeria-12.webp">
+              <img class="img-fluid" src="../imagem/pixel/galeria/galeria-12.webp" alt="Desenho grupo do Peecol">
               <figcaption class="text-center pt-1">Peecol de eBoy</figcaption>
             </figure>
           </div>
@@ -88,20 +89,20 @@ include '../include/header.php'; ?>
         <div class="ilust">
           <div class="align-self-end overlay ">
             <figure>
-              <img class="img-fluid align-self-center" src="../imagem/pixel/adventuretime.webp">
+              <img class="img-fluid align-self-center" src="../imagem/pixel/adventuretime.webp" alt="desenho da abertura do Adventure Time">
               <figcaption class="text-center pt-1">Adventure Time de Ivan Dixon</figcaption>
             </figure>
           </div>
           <div class="align-self-end overlay">
             <figure>
-              <img class="img-fluid" src="../imagem/pixel/simpsons.webp">
+              <img class="img-fluid" src="../imagem/pixel/simpsons.webp" alt="Abertura do desenho do The Simpsons">
               <figcaption class="text-center pt-1">Os Simpsons de Ivan Dixon</figcaption>
             </figure>
           </div>
 
           <div class="align-self-end overlay">
             <figure>
-              <img class="img-fluid" src="../imagem/pixel/galeria/galeria-10.webp">
+              <img class="img-fluid" src="../imagem/pixel/galeria/galeria-10.webp" alt="Cartaz do filme Pixels">
               <figcaption class="text-center pt-1">Filme Pixels de Chris Columbus</figcaption>
             </figure>
           </div>
@@ -109,7 +110,6 @@ include '../include/header.php'; ?>
         </div>
     </section>
   </div>
-
 </div>
 <?php include '../include/footer.php' ?>
 <?php include '../include/nossosLinksBotom.php' ?>
