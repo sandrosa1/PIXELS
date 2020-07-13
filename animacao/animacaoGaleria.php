247f65a7668b799bd5c0140b1ce2d45e2df00561
Sandro Amâncio de Sá <sandro.sa@fatec.sp.gov.br>
2020-07-13 18:36:55 -0300

    Galeria

--- animacao/animacaoGaleria.php
+++ animacao/animacaoGaleria.php
@@ -12,28 +12,28 @@
     <h2 class=" text-center animacaoh1">Galeria de Imagens Animação digital</h2>
         <div class=" row imagensGaleria">
             <div class=" col-lg-3 galeria ">
-                <a><img src="../imagem/animacao/animacaoAlvyRaySmith.webp" alt="Foto de Alvy Ray"  onclick="myFunction(this);"></a>
-                <a><img src="../imagem/animacao/animacaoWalter.webp" alt="Imagem de walter criador do desenho pica-pau" onclick="myFunction(this);"></a>
+                <a><img src="../imagem/animacao/animacaoAlvyRaySmith.webp" alt="Foto de Alvy Ray"  onclick="aumetaImagem(this);"></a>
+                <a><img src="../imagem/animacao/animacaoWalter.webp" alt="Imagem de walter criador do desenho pica-pau" onclick="aumetaImagem(this);"></a>
                 
 
             </div>
             <div class=" col-lg-3 galeria ">
-                <a><img src="../imagem/animacao/animacaoWallt.webp" alt="Imagem de WALT DISNEY " onclick="myFunction(this);"></a>
-                <a><img src="../imagem/animacao/animacaoMax.webp" alt="MAX FLEISCHER inventor do  o Rotoscópio." onclick="myFunction(this);"></a>
+                <a><img src="../imagem/animacao/animacaoWallt.webp" alt="Imagem de WALT DISNEY " onclick="aumetaImagem(this);"></a>
+                <a><img src="../imagem/animacao/animacaoMax.webp" alt="MAX FLEISCHER inventor do  o Rotoscópio." onclick="aumetaImagem(this);"></a>
             </div>
             <div class=" col-lg-3 galeria ">
-                <a><img src="../imagem/animacao/animacaoWilson.webp" alt="Winsor McCay Sua técnica para desenhar era tão apurada, tão diferente, que serviria como base para diversos animadores que surgiriam depois como Tex Avery, Walter Lantz, até mesmo Walt Disney." onclick="myFunction(this);" ></a>
-                <a><img src="../imagem/animacao/animacaoHayao.webp" alt="Hayao Miyazaki Começou a desenhar em 1958, quando fazia Ciências Políticas e Economia na Universidade Gakushuin" onclick="myFunction(this);" ></a>
+                <a><img src="../imagem/animacao/animacaoWilson.webp" alt="Winsor McCay Sua técnica para desenhar era tão apurada, tão diferente, que serviria como base para diversos animadores que surgiriam depois como Tex Avery, Walter Lantz, até mesmo Walt Disney." onclick="aumetaImagem(this);" ></a>
+                <a><img src="../imagem/animacao/animacaoHayao.webp" alt="Hayao Miyazaki Começou a desenhar em 1958, quando fazia Ciências Políticas e Economia na Universidade Gakushuin" onclick="aumetaImagem(this);" ></a>
 
                 
             </div>
             <div class=" col-lg-3 galeria ">
-                <a><img src="../imagem/animacao/animacaoSteve.webp" alt="Imagem de Steve Jobs quando tornou-se o maior acionista individual da Disney" onclick="myFunction(this);"></a>
-                <a><img src="../imagem/animacao/animacaoJohnAlanLasseter.webp" alt="Imagem de John Alan Lasseter conhecido por ser o diretor de criação da Pixar, Walt Disney Animation Studios" onclick="myFunction(this);" ></a>
-                <a><img src="../imagem/animacao/animacaoDupla.webp" alt="Fot de William Hanna e Joseph Barbera dupal que criou tom e jerry" onclick="myFunction(this);"></a>
+                <a><img src="../imagem/animacao/animacaoSteve.webp" alt="Imagem de Steve Jobs quando tornou-se o maior acionista individual da Disney" onclick="aumetaImagem(this);"></a>
+                <a><img src="../imagem/animacao/animacaoJohnAlanLasseter.webp" alt="Imagem de John Alan Lasseter conhecido por ser o diretor de criação da Pixar, Walt Disney Animation Studios" onclick="aumetaImagem(this);" ></a>
+                <a><img src="../imagem/animacao/animacaoDupla.webp" alt="Fot de William Hanna e Joseph Barbera dupal que criou tom e jerry" onclick="aumetaImagem(this);"></a>
             </div>
-            <script>
-                    function myFunction(imgs) 
+            <script type="text/javascript">
+                    function aumetaImagem(imgs) 
                     {
                         var expandImg = document.getElementById("expandedImg");
                         var imgText = document.getElementById("imgtext");
