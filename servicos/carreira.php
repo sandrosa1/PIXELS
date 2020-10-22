5aa7560c8a9d385dcfceda50a3f1ed35d4d1b156
Carolina <carolina.kaji@gmail.com>
2020-10-22 17:41:25 -0300

    Acessibilidade: Ajuste carreira.php

--- servicos/carreira.php
+++ servicos/carreira.php
@@ -17,32 +17,32 @@
         <hr>
         <div class="form-group mb-3">
           <label for="nome">Nome:</label>
-          <input type="text" name="nome" id="nome" class="form-control" placeholder="ex: João" required>
+          <input type="text" name="nome" id="nome" class="form-control" required aria-required="true"  placeholder="exemplo: João" >
         </div>
         <div class="form-group mb-3">
           <label for="sobrenome">Sobrenome:</label>
-          <input type="text" name="sobrenome" id="sobrenome" class="form-control" placeholder="ex: Silva" required>
+          <input type="text" name="sobrenome" id="sobrenome" class="form-control" placeholder="exemplo: Silva" required  aria-required="true">
         </div>
         <div class="form-group mb-3">
           <label for="nascimento">Data de Nascimento:</label>
-          <input type="date" name="nascimento" id="nascimento" class="form-control" required>
+          <input type="date" name="nascimento" id="nascimento" class="form-control" required aria-required="true">
         </div>
 
         <h5 class="mt-3">Informações para contato</h5>
         <hr>
         <div class="form-group mb-3">
           <label for="endereco">Endereço:</label>
-          <input type="text" name="endereco" id="endereco" class="form-control" placeholder="ex: Rua Marechal Deodoro da Fonseca, 132" required>
+          <input type="text" name="endereco" id="endereco" class="form-control" placeholder="exemplo: Rua Marechal Deodoro da Fonseca, 132" required aria-required="true">
         </div>
         <div class="form-row">
           <div class="form-group col-sm-7 mb-3">
             <label for="cidade">Cidade:</label>
-            <input type="text" name="cidade" id="cidade" class="form-control" placeholder="ex: São Roque" required>
+            <input type="text" name="cidade" id="cidade" class="form-control" placeholder="exemplo: São Roque" required aria-required="true">
           </div>
           <div class="form-group col-sm-5 mb-3">
             <label for="estado">Estado:</label>
             <select name="estado" id="estado" class="form-control">
-              <option value="">Selcione</option>
+              <option value="">Selecione</option>
               <option value="AC">Acre</option>
               <option value="AL">Alagoas</option>
               <option value="AP">Amapá</option>
@@ -76,11 +76,11 @@
         <div class="form-row">
           <div class="form-group col-sm-6 mb-3">
             <label for="telefone">Telefone:</label>
-            <input type="tel" name="telefone" id="telefone" class="form-control" placeholder="ex: (11) 4712-3229" required>
+            <input type="tel" name="telefone" id="telefone" class="form-control" placeholder="exemplo: DDD + 4712-3229" required aria-required="true">
           </div>
           <div class="form-group col-sm-6 mb-3">
             <label for="celular">Celular:</label>
-            <input type="tel" name="celular" id="celular" class="form-control" placeholder="ex: (11) 94712-3229">
+            <input type="tel" name="celular" id="celular" class="form-control" placeholder="exemplo:  DDD + 94712-3229">
           </div>
         </div>
 
@@ -88,7 +88,7 @@
         <hr>
         <div class="form-group mb-3">
           <label for="empresa">Empresa:</label>
-          <input type="text" name="empresa" id="empresa" class="form-control" placeholder="exemmplo: Accenture Brasil">
+          <input type="text" name="empresa" id="empresa" class="form-control" placeholder="exemplo: Accenture Brasil">
         </div>
         <div class="form-group mb-3">
           <label for="cargo">Cargo:</label>
@@ -103,7 +103,7 @@
         <hr>
         <div class="form-group mb-3">
           <label for="universidade">Universidade:</label>
-          <input type="text" name="universidade" id="universidade" class="form-control" placeholder="exemplo: Fatec São Roque" required>
+          <input type="text" name="universidade" id="universidade" class="form-control" placeholder="exemplo: Fatec São Roque" required aria-required="true"> 
         </div>
         <div class="form-row mb-3">
           <div class="form-group col-sm-4 mb-3">
