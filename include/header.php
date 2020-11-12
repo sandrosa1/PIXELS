4f2e3be4d51bcabce16df1ab7693dd59c51e4a0a
Carolina <carolina.kaji@gmail.com>
2020-11-12 19:15:53 -0300

    menu para direita

--- include/header.php
+++ include/header.php
@@ -108,7 +108,7 @@ switch ($page) {
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
                     <a class="nav-link" href="#conteudo">Ir ao Conteúdo<span class="sr-only">(current)</span></a>
                 </li>
